18cc031186a1542a813127371112072c709619d0
mejdrhaiem <mejd.rhaiem@polytechnicien.tn>
2026-08-13 04:24:11 +0100

    2145

--- app/admin/layout.tsx
+++
@@ -1,7 +0,0 @@
-export const dynamic = 'force-dynamic'
-
-import AdminLayout from '@/components/admin/AdminLayout'
-
-export default function AdminRootLayout({ children }: { children: React.ReactNode }) {
-  return <AdminLayout>{children}</AdminLayout>
-}
